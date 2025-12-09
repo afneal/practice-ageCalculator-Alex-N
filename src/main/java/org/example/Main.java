2637b7eb35a40d5a1bfa8c69ec2b5a16de160c40
afneal <Afneal0226@gmail.com>
2025-12-08 20:06:48 -0600

    completed assignment

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,19 @@
 package org.example;
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-    //test
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter your age: ");
+    int age = input.nextInt();
+
+    int yearsLeft = 100 - age;
+
+    System.out.println("You have " + yearsLeft +
+            " years until you are 100 years old.");
+
+    input.close();
 
     }
 }
